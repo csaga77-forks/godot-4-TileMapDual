c77ac7dd35fdba968df8d113d3079898fd5b5671
Raffi Molero <raffimolero@gmail.com>
2025-05-24 04:31:57 +0800

    make popups ad-hoc

--- addons/TileMapDual/plugin.gd
+++ addons/TileMapDual/plugin.gd
@@ -3,7 +3,6 @@ class_name TileMapDualEditorPlugin
 extends EditorPlugin
 
 static var instance: TileMapDualEditorPlugin = null
-var _popup: AcceptDialog
 
 
 # TODO: create a message queue that groups warnings, errors, and messages into categories
@@ -11,7 +10,7 @@ var _popup: AcceptDialog
 
 
 func _enter_tree() -> void:
-	_init_instance()
+	instance = self
 	add_custom_type("TileMapDual", "TileMapLayer", preload("TileMapDual.gd"), preload("TileMapDual.svg"))
 	add_custom_type("CursorDual", "Sprite2D", preload("CursorDual.gd"), preload("CursorDual.svg"))
 	add_custom_type("TileMapDualLegacy", "TileMapLayer", preload("TileMapDualLegacy.gd"), preload("TileMapDual.svg"))
@@ -19,25 +18,19 @@ func _enter_tree() -> void:
 
 
 func _exit_tree() -> void:
+	instance = null
 	remove_custom_type("CursorDual")
 	remove_custom_type("TileMapDual")
 	remove_custom_type("TileMapDualLegacy")
-	_deinit_instance()
 	print("plugin TileMapDual unloaded")
 
 
-func _init_instance() -> void:
-	_popup = AcceptDialog.new()
-	_popup.name = 'ErrorPopup'
-	get_editor_interface().get_base_control().add_child(_popup)
-	instance = self
-
-
-func _deinit_instance() -> void:
-	_popup.queue_free()
-
-
 static func popup(title: String, message: String) -> void:
-	instance._popup.title = title
-	instance._popup.dialog_text = message
-	instance._popup.popup_centered()
+	var popup := AcceptDialog.new()
+	instance.get_editor_interface().get_base_control().add_child(popup)
+	popup.name = 'TileMapDualPopup'
+	popup.title = title
+	popup.dialog_text = message
+	popup.popup_centered()
+	await popup.confirmed
+	popup.queue_free()
